5e376b704e29f2af06e0d900abf9585cd15ce12a
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-20 21:47:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,16 +17,12 @@ end
 
 
 def word_substituter(string)
-  shortened_string = ""
   string_to_array = string.split
   string_to_array.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
-      shortened_string << word
     else
       word
-      shortened_string << word
-    end
+    end.join(" ")
   end
-  shortened_string
 end
