b676675787d32945a1aff07574e97af7fd328e9c
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-20 21:30:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def dictionary
+  {"hello": "hi",
+  "to": "2",
+  "two": "2",
+  "too": "2",
+  "for": "4",
+  "four": "4",
+  "be": "b",
+  "you": "u",
+  "at": "@",
+  "and": "&"
+  }
