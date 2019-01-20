06239de4b94402e03ee5c1b2b1af44b8a8e11f2e
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-20 21:32:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,17 +1,18 @@
 # Write your code here.
 
 def dictionary
-  {"hello": "hi",
-  "to": "2",
-  "two": "2",
-  "too": "2",
-  "for": "4",
-  "four": "4",
-  "be": "b",
-  "you": "u",
-  "at": "@",
-  "and": "&"
-  }
+  dictionary = {
+    "hello": "hi",
+    "to": "2",
+    "two": "2",
+    "too": "2",
+    "for": "4",
+    "four": "4",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
+    }
 end
 
 
