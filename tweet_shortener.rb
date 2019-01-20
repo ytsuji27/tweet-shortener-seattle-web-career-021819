40df1c2557022f8900469e28e8cbabe1e06b32b9
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-20 22:31:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,3 +33,12 @@ def bulk_tweet_shortener(tweet_array)
     puts word_substituter(tweet)
   end
 end
+
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    puts word_substituter(tweet)
+  else
+    puts tweet
+  end
+end
